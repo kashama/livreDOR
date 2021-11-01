589797ff6daf7c385e77135f26337cd8798031e2
kashama <bijkashama2@gmail.com>
2021-11-01 20:54:38 +0100

    exemple d'obervable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, range } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',//permet d'utiliser ce coomposant partout dans l'application
   templateUrl: './app.component.html',// template html du composant
   styleUrls: ['./app.component.css']//partie de styles css du composant
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'LivresDOR';//proprieté de la classe AppComponent
+
+  source$:Observable<number>=range(0, 10);
+  ngOnInit(): void {
+    this.source$.pipe(
+      map(x=>x*4),
+      filter(x=>x>15)
+    ).subscribe(x=>console.log(x))
+  }
+  
 }
